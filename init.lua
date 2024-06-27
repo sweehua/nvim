46f50466cae27c3e5569cd9e7bba715f267e912a
sweehua.law <>
2024-06-27 14:35:31 +0800

    add verilog linter

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- https://chipsalliance.github.io/verible/verilog_lint.html
+-- ~/.local/share/nvim/mason/bin/verible-verilog-lint --rules_config ~/.config/nvim/extra/verible/.rules.verible_lint --print_rules_file
+require("lspconfig").verible.setup({
+    cmd = {
+        "verible-verilog-ls",
+        "--rules_config",
+        "~/.config/nvim/extra/verible/.rules.verible_lint",
+    },
+})
